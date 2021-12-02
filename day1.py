6779689d4c0eede71a5ce6e4a35d88f895dc8020
fuge <fuge@fuge>
2021-12-02 19:03:26 +0100

    day 1 part 2 added

--- day1.py
+++ day1.py
@@ -1,9 +1,26 @@
 
+#%%
 import numpy as np
 
+# part 1
 input = np.loadtxt("day1_input.txt")
-diff = np.diff(input)
-increased = diff > 0 
-count_increased = np.sum(increased)
-print(count_increased)
 
+def get_increase_count(data):
+    """Returns the number of times the data increases"""
+    diff = np.diff(data)
+    increased = diff > 0 
+    count_increased = np.sum(increased)
+    return count_increased
+
+print(get_increase_count(input))
+
+#%%
+
+# part 2
+
+w = 3 # window size
+rolling_avg = np.convolve(input, np.ones(w), 'valid') / w
+
+print(get_increase_count(rolling_avg))
+
+# %%
